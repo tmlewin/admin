b51548e67e940fc9ce8a7335371847267f751bf3
tmlewin <dexterity4u@gmail.com>
2022-04-04 16:47:56 -0500

    "update"

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import './home.scss'
+import Sidebar from '../../components/sidebar/Sidebar'
 
 const Home = () => {
   return (
-    <div className="home">Home</div>
+    <div className="home">
+      <Sidebar/>
+      <div className="homeContainer">container</div>
+    </div>
   )
 }
 
