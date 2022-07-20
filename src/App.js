230331ee23703508e7a527d41f1a76fddd0eed4a
tmlewin <dexterity4u@gmail.com>
2022-07-19 21:52:32 -0500

    "update"

--- src/App.js
+++ src/App.js
@@ -5,19 +5,27 @@ import List from './pages/list/List';
 import Single from './pages/single/Single';
 import New from './pages/new/New';
 import {BrowserRouter,Routes, Route} from 'react-router-dom';
+import { productInputs, userInputs } from "./formSource";
+import './style/dark.scss'
+import {DarkModeContext} from './context/darkModeContext';
+import {useContext} from 'react';
+
+
 
 function App() {
+  const {darkMode} = useContext(DarkModeContext);
+  
   return (
-    <div className="App">
+    <div className={darkMode ? 'app dark' : 'app'}>
        <BrowserRouter>
     <Routes>
       <Route path="/" >
-        <Route index element={<Home />} />
-        <Route path="login" element={<Login />} />
+        <Route index element={<Home  />} />
+        <Route path="login" element={<Login />}  />
         <Route path="users">
           <Route index element={<List />} />
           <Route path=":userId" element={<Single />} />
-          <Route path="new" element={<New />} />
+          <Route path="new" element={<New inputs={userInputs} />} />
 
 
 
@@ -25,7 +33,7 @@ function App() {
         <Route path="products">
           <Route index element={<List />} />
           <Route path=":productId" element={<Single />} />
-          <Route path="new" element={<New />} />
+          <Route path="new" element={<New inputs={productInputs} />} />
 
 
 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import './home.scss'
 import Sidebar from '../../components/sidebar/Sidebar'
 import Navbar from '../../components/navbar/Navbar'
+import Widget from '../../components/widgets/Widget'
+import Featured from '../../components/featured/Featured'
+import Chart from '../../components/chart/Chart'
+import Table from '../../components/table/Table'
 
 
 const Home = () => {
@@ -10,7 +14,21 @@ const Home = () => {
       <Sidebar/>
       <div className="homeContainer">
         <Navbar/>
-        home container
+       <div className="widgets">
+        <Widget type="user"/>
+        <Widget type="order"/>
+        <Widget type="earning"/>
+        <Widget type="balance"/>
+       </div>
+       <div className="charts">
+        <Featured/>
+        <Chart title="User Spending ( Last 6 Months)" aspect={ 2/1}/>
+
+       </div>
+       <div className="listContainer">
+        <div className="listTitle">Latest Transactions</div>
+        <Table/>
+       </div>
       </div>
     </div>
   )
